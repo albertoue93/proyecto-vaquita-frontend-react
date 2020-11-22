23c40034933c7a47cb6711b27fedf7cc23c7fe0d
Miguelnz30 <miguelcanales72@hotmail.com>
2020-11-22 17:30:19 -0600

    test

---
+++ src/components/animales/__test__/anima.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Animal,{animales} from './../index'
+import { render, screen } from '@testing-library/react';
+it("renders without crashing", () =>{
+    const div = document.createElement("div");
+    ReactDOM.render(<Animal></Animal>,div)
+  })
